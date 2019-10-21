26afdf92b2ddbaa9ac9440a3fda5a1642ed13c61
Agra <agra@kerjaku.net>
2019-10-22 01:55:43 +0700

    fix Header

--- lib/data/api_service.dart
+++ lib/data/api_service.dart
@@ -8,50 +8,54 @@ Map<String, String> params = {};
 Map<String, String> body = {};
 Response response;
 FormData formData;
+Dio dio;
 
 class ApiService {
-
   static String BASE_URL = "http://premier.pragmainf.tech";
 
   /////////////// END POINT //////////////////////////
   static login(
       {@required BuildContext context,
-        @required String email,
-        @required String password}) async {
+      @required String email,
+      @required String password}) async {
     params.clear();
-    Dio dio =  _dioSetting(false);
+    await _dioSetting(false);
     params['username'] = email;
     params['password'] = password;
-    print(params.toString());
-    return response = await dio.post("/auth/login", data: params);
+//    print(params.toString());
+    response = await dio.post("/auth/login", data: params);
+//    print(response);
+    return response;
   }
 
-  static getReport(
-      {@required BuildContext context}) async {
+  static getReport({@required BuildContext context}) async {
     params.clear();
-    try{
-      Dio dio =  _dioSetting(true);
-      return response = await dio.get("/report");
-    }on DioError catch (e) {
+    try {
+      Dio dio = await _dioSetting(true);
+      response = await dio.get("/report");
+//      print("AAA" + response.toString());
+      return response;
+    } on DioError catch (e) {
       return ReportResponse.fromJson({'message': e.message});
     }
   }
 
   ///////////////// Settings Dio /////////////////////////////
-  static _dioSetting(bool isHeader){
-    Dio dio = new Dio();
-    dio.options.baseUrl = BASE_URL;
-    dio.options.connectTimeout = 5000; //5s
-    dio.options.receiveTimeout = 3000;
-    if (isHeader)dio.options.headers = getHeaders();
+  static _dioSetting(bool isHeader) async {
+    dio = new Dio();
+//    dio.options.baseUrl = BASE_URL;
+//    dio.options.connectTimeout = 5000; //5s
+//    dio.options.receiveTimeout = 3000;
+    if (isHeader) dio.options.headers = await getHeaders();
 
     BaseOptions options = new BaseOptions(
         baseUrl: BASE_URL,
         connectTimeout: 5000,
         receiveTimeout: 3000,
         followRedirects: false,
-        validateStatus: (status) { return status < 500;}
-    );
+        validateStatus: (status) {
+          return status < 500;
+        });
     return dio = new Dio(options);
   }
 
@@ -59,12 +63,12 @@ class ApiService {
   static getHeaders() async {
     Map<String, String> header = {};
     header.clear();
+    String token = await Session.getToken();
     header = {
-      "Authorization": "Bearer " + await Session.getToken(),
+      "Authorization": "Bearer " + token,
       "Accept": "application/json",
     };
     print("HEADERSS: " + header.toString());
     return header;
   }
-
 }
--- lib/pages/views/student.dart
+++ lib/pages/views/student.dart
@@ -17,94 +17,94 @@ class _StudentActivityState extends State<StudentActivity> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          fontFamily: 'Roboto'),
+      theme: ThemeData(fontFamily: 'Roboto'),
       home: Scaffold(
         appBar: AppBar(
-         title: Text('List Report'),
+          title: Text('List Report'),
           backgroundColor: MyColor.skyBlue,
         ),
         body: Container(
-            child: Column(
-              children: <Widget>[
-                FutureBuilder<List<Data>>(
-                    /// ini panggil API dengan snapshotnya
-                    future: getReport(),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasError) {
+          child: Column(
+            children: <Widget>[
+              FutureBuilder<List<Data>>(
+
+                  /// ini panggil API dengan snapshotnya
+                  future: getReport(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Container(
+                        height: 100,
+                        child: Center(child: Text(snapshot.error.toString())),
+                      );
+                    }
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
                         return Container(
-                          height: 100,
-                          child: Center(
-                              child: Text(snapshot.error.toString())),
+                          margin: EdgeInsets.only(top: 20),
+                          child: Center(child: CircularProgressIndicator()),
                         );
-                      }
-                      switch (snapshot.connectionState) {
-                        case ConnectionState.waiting:
-                          return Container(
-                            margin: EdgeInsets.only(top: 20),
-                            child: CircularProgressIndicator(),
-                          );
-                        case ConnectionState.done:
-                          if (snapshot.hasData) {
-                            if (snapshot.data.length == 0) {
-                              return Container(
-                                margin: EdgeInsets.only(top: 20),
-                                child: Text("Data tidak ditemukan"),
-                              );
-                            } else {
-                              ///Dimasukan ke Item (Adapter)
-                              return ItemReport(report : snapshot.data);
-                            }
-                          } else {
+                      case ConnectionState.done:
+                        if (snapshot.hasData) {
+                          if (snapshot.data.length == 0) {
                             return Container(
-                              height: 100,
-                              child: Center(child: Text("Siswa tidak ditemukan")),
+                              margin: EdgeInsets.only(top: 20),
+                              child: Text("Data tidak ditemukan"),
                             );
+                          } else {
+                            ///Dimasukan ke Item (Adapter)
+                            return ItemReport(report: snapshot.data);
                           }
-                          break;
-                        default:
-                          return Container();
-                      }
-                    }),
-              ],
-            ),
+                        } else {
+                          return Container(
+                            height: 100,
+                            child: Center(child: Text("Data tidak ditemukan")),
+                          );
+                        }
+                        break;
+                      default:
+                        return Container();
+                    }
+                  }),
+            ],
+          ),
         ),
       ),
     );
   }
 
   ///Untuk Get Api
-  /*Future<List<Data>> getReport() async {
-    final ReportResponse responses = await ApiService.getReport(
-        context: context);
-    await Future<String>.delayed(const Duration(milliseconds: 300));
-    if (responses.data != null) {
-      var res = new Map<String, dynamic>.from(responses.data);
-      ReportResponse reportList = ReportResponse.fromJson(res);
-      return reportList.data;
-    } else {
-      Fluttertoast.showToast(
-          msg: "Kesalahan : "+responses.toString(), toastLength: Toast.LENGTH_SHORT);
-      return null;
-    }
-  }*/
-
-  ///Untuk Get Api
   Future<List<Data>> getReport() async {
-    final Response responses = await ApiService.getReport(
-        context: context);
+    final Response responses = await ApiService.getReport(context: context);
     await Future<String>.delayed(const Duration(milliseconds: 300));
-    if (responses.statusCode == APIResponseCode.SUCCESS) {
-      var res = new Map<dynamic, dynamic>.from(response.data);
-      ReportResponse reportList = ReportResponse.fromJson(res);
-      print(responses.data);
+    if (responses.data != null &&
+        responses.statusCode == APIResponseCode.SUCCESS) {
+//      var res = new Map<String, dynamic>.from(responses.data);
+      ReportResponse reportList = ReportResponse.fromJson(responses.data);
       return reportList.dataList;
     } else {
       Fluttertoast.showToast(
-          msg: "Kesalahan : "+responses.statusCode.toString(), toastLength: Toast.LENGTH_SHORT);
+          msg: "Kesalahan : " + responses.toString(),
+          toastLength: Toast.LENGTH_SHORT);
       return null;
     }
   }
+
+  ///Untuk Get Api
+//  Future<List<Data>> getReport() async {
+//    final Response responses = await ApiService.getReport(
+//        context: context);
+//    await Future<String>.delayed(const Duration(milliseconds: 300));
+//    if (responses.statusCode == APIResponseCode.SUCCESS) {
+//      var res = new Map<dynamic, dynamic>.from(response.data);
+//      ReportResponse reportList = ReportResponse.fromJson(res);
+//      print(responses.data);
+//      return reportList.dataList;
+//    } else {
+//      Fluttertoast.showToast(
+//          msg: "Kesalahan : "+responses.statusCode.toString(), toastLength: Toast.LENGTH_SHORT);
+//      return null;
+//    }
+//  }
 
   ///kalau add list static//////
   /*List GetStudent(){
